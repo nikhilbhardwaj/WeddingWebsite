99f49b3e73b6460ac8f215218f56083c3b1877f9
Nikhil Bhardwaj <ROOT@NikhilBhardwaj.in>
2015-02-14 18:32:48 -0500

    Integrate with twitter bootstrap. Add barebones RSVP form. Integrate with Facebook.

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ ruby "2.2.0"
 gem 'unicorn'
 gem 'sinatra'
 gem 'sinatra-static-assets'
+gem 'koala'
+gem 'dotenv-rails', :groups => [:development, :test]
--- app/weddingsite.rb
+++ app/weddingsite.rb
@@ -1,11 +1,22 @@
 require 'sinatra/base'
 require 'sinatra/static_assets'
+require 'koala'
 
 class WeddingSite < Sinatra::Base
+  # Load credentials from .env during development
+  if settings.development?
+    require 'dotenv'
+    Dotenv.load
+  end
   register Sinatra::StaticAssets
+  use Rack::Session::Cookie, secret: ENV['FACEBOOK_SECRET']
 
   get '/' do
     erb :index
+  end
+
+  get '/rsvp' do
+    erb :rsvp
   end
 
   get '/photos' do
@@ -14,6 +25,25 @@ class WeddingSite < Sinatra::Base
 
   get '/gifts' do
     'Here\'s what you should gift us :)'
+  end
+
+  get '/login' do
+    # generate a new oauth object with your app data and your callback url
+    session['oauth'] = Koala::Facebook::OAuth.new(ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'], "#{request.base_url}/callback")
+    # redirect to facebook to get your code
+    redirect session['oauth'].url_for_oauth_code()
+  end
+
+  get '/logout' do
+    session.clear
+    redirect '/'
+  end
+
+  #method to handle the redirect from facebook back to us
+  get '/callback' do
+    #get the access token from facebook with your code
+    session['access_token'] = session['oauth'].get_access_token(params[:code])
+    redirect '/'
   end
 
   private
